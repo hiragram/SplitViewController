1e1ffaef9547f6e657a6b21d4cbe3f7ae13fe14d
Yuya Hirayama <y@yura.me>
2017-03-10 01:19:05 +0900

    Initial Commit

---
+++ SplitViewController/SplitViewController.h
@@ -0,0 +1,19 @@
+//
+//  SplitViewController.h
+//  SplitViewController
+//
+//  Created by Yuya Hirayama on 2017/03/10.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SplitViewController.
+FOUNDATION_EXPORT double SplitViewControllerVersionNumber;
+
+//! Project version string for SplitViewController.
+FOUNDATION_EXPORT const unsigned char SplitViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SplitViewController/PublicHeader.h>
+
+
